bbdd2a74ca7c73ea9e2b18dd7c3dd127034e48d8
ROHAN HARICHANDAN <72160268+RohanHarichandan@users.noreply.github.com>
2022-04-06 17:10:53 +0530

    Time: 34 ms (52.02%), Space: 41.8 MB (84.68%) - LeetHub

---
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int threeSumMulti(int[] A, int target) {
+        int MOD = 1_000_000_007;
+        long ans = 0;
+        Arrays.sort(A);
+
+        for (int i = 0; i < A.length; ++i) {
+
+            int T = target - A[i];
+            int j = i+1, k = A.length - 1;
+
+            while (j < k) {
+               
+                if (A[j] + A[k] < T)
+                    j++;
+                else if (A[j] + A[k] > T)
+                    k--;
+                else if (A[j] != A[k]) { 
+                    int left = 1, right = 1;
+                    while (j+1 < k && A[j] == A[j+1]) {
+                        left++;
+                        j++;
+                    }
+                    while (k-1 > j && A[k] == A[k-1]) {
+                        right++;
+                        k--;
+                    }
+
+                    ans += left * right;
+                    ans %= MOD;
+                    j++;
+                    k--;
+                } else {
+                    
+                    ans += (k-j+1) * (k-j) / 2;
+                    ans %= MOD;
+                    break;
+                }
+            }
+        }
+
+        return (int) ans;
+    }
+}
