6c86b3e97ad8691a378d0306dba0668ad56de30b
ROHAN HARICHANDAN <72160268+RohanHarichandan@users.noreply.github.com>
2022-09-11 05:56:54 +0530

    Time: 20 ms (7.60%), Space: 41.5 MB (9.46%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,8 +1,9 @@
 class Solution {
     public int fib(int n) {
         
-        if(n<2)
-            return n;
-        return (fib(n-1)+fib(n-2));
-    }
+      if(n<2){
+          return n;
+      }
+        return fib(n-1)+fib(n-2);
+        }
 }
