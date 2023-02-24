76c916684cdb3b75114b639de9dc690946550886
ROHAN HARICHANDAN <72160268+RohanHarichandan@users.noreply.github.com>
2023-02-24 21:27:07 +0530

    Time: 118 ms (84.91%), Space: 51.8 MB (20.75%) - LeetHub

---
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minimumDeviation(int[] nums) {
+     
+         TreeSet<Integer> set = new TreeSet<>();
+        for (int x : nums)
+            if (x%2 == 0)
+                set.add(x);
+            else
+                set.add(x*2);
+        int ans = Integer.MAX_VALUE;
+        while (true) {
+            int val = set.last();
+            ans = Math.min(ans, val - set.first());
+            if (val%2 == 0) {
+                set.remove(val);
+                set.add(val/2);
+            } else
+                break;
+        }
+        return ans;
+    }
+}
