9340619e40f2fee8f3b91193940cffac2fb24c85
ROHAN HARICHANDAN <72160268+RohanHarichandan@users.noreply.github.com>
2023-05-12 21:04:39 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (39.26%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String toLowerCase(String s) {
+        
+        if(s==null){
+            return s;
+        }
+
+       return s.toLowerCase();
+        // for(int i=0;i<s.length();i++){
+        //     if(s.charAt(i) <97){
+        //         //char a=s.charAt(i);
+        //         //char b=(char)(a+32);
+        //         //s.replace(a,b);
+        //         s.charAt(i).toLowerCase();
+        //     }
+        // }
+        
+        
+    }
+}
