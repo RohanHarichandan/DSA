94750995930372ed229e1aa05d9e0e68e0541be9
ROHAN HARICHANDAN <72160268+RohanHarichandan@users.noreply.github.com>
2023-06-07 16:25:11 +0530

    Added solution - LeetHub

---
+++ Cyclically rotate an array by one - GFG/cyclically-rotate-an-array-by-one.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Compute obj = new Compute();
+            obj.rotate(a, n);
+            
+            StringBuilder output = new StringBuilder();
+            for(int i=0;i<n;i++)
+                output.append(a[i]+" ");
+            System.out.println(output);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Compute {
+    
+    static void rev(int arr[],int i){
+        
+        int j=arr.length-1;
+        while(i<j){
+           int temp=arr[i];
+           arr[i]=arr[j];
+           arr[j]=temp;
+           i++;
+           j--;
+        }
+      
+    }
+    
+    public void rotate(int arr[], int n)
+    {
+        rev(arr,0);
+        rev(arr,1);
+        //System.out.println(Arrays.toString(x));
+    }
+}
